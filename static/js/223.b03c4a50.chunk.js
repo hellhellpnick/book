4d5a3972a6e30342324961d39367572145660e6f
Alex <hellhellpnick@gmail.com>
2024-08-11 02:22:25 +0300

    Updates

--- static/js/223.b03c4a50.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfront=self.webpackChunkfront||[]).push([[223],{1223:function(t,e,n){n.r(e);n(2791);var r=n(3012),s=n(9464),u=n(184);e.default=function(t){var e=t.title,n=t.show;return(0,(0,r.Z)().ChangeTitle)(e),(0,u.jsx)(s.Z,{show:n})}}}]);
-//# sourceMappingURL=223.b03c4a50.chunk.js.map
